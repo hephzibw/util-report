670f4776e294a4b1abcd3ac198fa2f5cefe1927f
Hephzibah <hephzibah.1989@gmail.com>
2014-04-29 00:34:22 +0530

    prints for debugging

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,10 +5,14 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     p "1"*100
 
     user = User.from_omniauth(request.env["omniauth.auth"])
+    p "5"*100
+    p user, user.persisted?
     if user.persisted?
+      p "6"*100
       flash.notice = "Signed in Through Google!"
       sign_in_and_redirect user
     else
+      p "7"*100
       session["devise.user_attributes"] = user.attributes
       flash.notice = "You are almost Done! Please provide a password to finish setting up your account"
       redirect_to new_user_registration_url
