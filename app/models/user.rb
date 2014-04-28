a12b9fc8a2bc9885da1864355e740ff75f65b847
Hephzibah <hephzibah.1989@gmail.com>
2014-04-29 00:22:31 +0530

    Removed prints, added hd restriction

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,14 +41,11 @@ class User
 
   def self.from_omniauth(auth)
     if user = User.find_by(email: auth.info.email)
-      p "2"*100
       user.provider = auth.provider
       user.uid = auth.uid
       user.save!
     else
-      p "3"*100
       User.where(:provider => auth.provider, :uid => auth.uid).first_or_create do |user|
-        p "4"*100
         user.provider = auth.provider
         user.uid = auth.uid
         user.username = auth.info.name
