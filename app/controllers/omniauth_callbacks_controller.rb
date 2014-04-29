5731a51173c084d15728ac1a2fbece7ef1e3b352
Hephzibah <hephzibah.1989@gmail.com>
2014-04-29 10:49:47 +0530

    Skipped before filter for oauth controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,19 +1,12 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
-    p "1"*100
-    p request.env["omniauth.auth"]
-    p "1"*100
+  skip_before_filter :require_login
 
+  def google_oauth2
     user = User.from_omniauth(request.env["omniauth.auth"])
-    p "5"*100
-    p user, user.persisted?
     if user.persisted?
-      p "6"*100
       flash.notice = "Signed in Through Google!"
       sign_in_and_redirect user
     else
-      p "7"*100
-      session["devise.user_attributes"] = user.attributes
       flash.notice = "You are almost Done! Please provide a password to finish setting up your account"
       redirect_to new_user_registration_url
     end
